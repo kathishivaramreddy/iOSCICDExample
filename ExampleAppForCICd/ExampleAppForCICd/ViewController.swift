58633da628b6717a9a655f1e4f0bb2092bd448de
Shiva Ram Reddy <kathishivaram@gmail.com>
2020-05-30 12:58:48 +0530

    setup intial ci

---
+++ ExampleAppForCICd/ExampleAppForCICd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleAppForCICd
+//
+//  Created by Shiva Ram Reddy on 30/05/20.
+//  Copyright © 2020 Shiva Ram Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
